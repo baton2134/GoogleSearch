e58c7918a2d3c1b9da365a277ed35f63d1bb5509
baton2134 <96778561+baton2134@users.noreply.github.com>
2021-12-28 18:57:40 +0300

    Update smsbot.py

--- smsbot.py
+++ smsbot.py
@@ -38,7 +38,7 @@ class Browser:
         return
 
     def start(self):
-        self.driver = webdriver.Chrome()
+        self.driver = webdriver.Chrome(ChromeDriverManager())
         self.driver.implicitly_wait(self.implicit_wait_time)
         
 
